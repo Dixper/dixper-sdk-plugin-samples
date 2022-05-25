2dd9574d9bda1395bceb68e43525bc56ccabce2e
Luis M Torres <lm.torresrivera@gmail.com>
2022-05-25 18:46:29 +0200

    [WIP]

--- src/fortnite/click-hole.sample.js
+++ src/fortnite/click-hole.sample.js
@@ -1,7 +1,7 @@
 const images = [
   {
     name: 'hole',
-    url: 'https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/games/assets/CIRCULO_00021.png',
+    url: 'https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/spritesheets/scope-ring-challenge.json',
   },
   {
     name: 'bg',
@@ -12,7 +12,10 @@ const images = [
 const sprites = [];
 const sounds = [];
 
-const clicks = 0;
+const jumpKey = 57;
+const clickKey = 1;
+
+let clicks = 0;
 
 let onClickSub;
 
@@ -28,6 +31,8 @@ const dixperPluginSample = new DixperSDKLib({
 // PIXIJS INITILIZE
 
 dixperPluginSample.onPixiLoad = () => {
+  init();
+
   const timestampUntilSkillFinish = dixperPluginSample.context.skillEnd;
   const millisecondsToFinish = timestampUntilSkillFinish - Date.now();
   const interval = 1000;
@@ -40,16 +45,14 @@ dixperPluginSample.onPixiLoad = () => {
     interval,
     {
       position: {
-        x: DX_WIDTH / 2 + 100,
+        x: DX_WIDTH / 2,
         y: 100,
       },
       animationSpeed: 0.5,
     }
   );
 
-  timer.onTimerStart = () => {
-    init();
-  };
+  timer.onTimerStart = () => {};
 
   timer.onTimerFinish = () => {
     onClickSub.unsubscribe();
@@ -57,52 +60,75 @@ dixperPluginSample.onPixiLoad = () => {
 };
 
 const init = () => {
-  const backgroundTransparent = new PIXI.Sprite(
-    dixperPluginSample.pixi.resources.bg.texture
+  const bgHole = new PIXI.Sprite(dixperPluginSample.pixi.resources.bg.texture);
+  bgHole.width = 1920;
+  bgHole.height = 1080;
+  bgHole.x = DX_WIDTH / 2;
+  bgHole.y = DX_HEIGHT / 2;
+  bgHole.scale.x = 4;
+  bgHole.scale.y = 4;
+  bgHole.anchor.set(0.5);
+
+  const hole = new PIXI.AnimatedSprite(
+    dixperPluginSample.pixi.resources.hole.spritesheet.animations['Loop']
   );
-  backgroundTransparent.width = 1920;
-  backgroundTransparent.height = 1080;
-  backgroundTransparent.x = DX_WIDTH / 2;
-  backgroundTransparent.y = DX_HEIGHT / 2;
-  backgroundTransparent.scale.x = 4;
-  backgroundTransparent.scale.y = 4;
-  backgroundTransparent.anchor.set(0.5);
+  //   const hole = new PIXI.Sprite(dixperPluginSample.pixi.resources.hole.texture);
+  hole.loop = false;
+  hole.width = 1000;
+  hole.height = 1000;
+  hole.scale.x *= 0.25;
+  hole.scale.y *= 0.25;
+  hole.anchor.set(0.5);
+  hole.play();
+  hole.animationSpeed = 0.5;
 
-  //   const backgroundTransparentChild = new PIXI.Sprite(
-  //     dixperPluginSample.pixi.resources.bg.texture
-  //   );
-  //   backgroundTransparentChild.width = 1920;
-  //   backgroundTransparentChild.height = 1080;
-  //   backgroundTransparentChild.scale.x *= 0.5;
-  //   backgroundTransparentChild.scale.y *= 0.5;
-  //   backgroundTransparentChild.anchor.set(0.5);
-
-  const scope = new PIXI.Sprite(dixperPluginSample.pixi.resources.hole.texture);
-  scope.width = 1000;
-  scope.height = 1000;
-  scope.scale.x *= 0.25;
-  scope.scale.y *= 0.25;
-  scope.anchor.set(0.5);
-
-  dixperPluginSample.pixi.stage.addChild(backgroundTransparent);
-  //   backgroundTransparent.addChild(backgroundTransparentChild);
-  backgroundTransparent.addChild(scope);
+  dixperPluginSample.uiLayer.addChild(bgHole);
+  bgHole.addChild(hole);
 
   const onClick = (event) => {
-    if (clicks < 5) {
-      console.log(event);
-      let increment = 0.75;
+    if (clickKey === event.button) {
+      if (clicks < 5) {
+        // hole.textures =
+        // dixperPluginSample.pixi.resources.hole.spritesheet.animations['Loop'];
+        // hole.play();
 
-      if (event.button === 2) {
-        increment = 1.25;
+        const tween = PIXI.tweenManager.createTween(bgHole);
+        tween.time = 5;
+        tween.repeat = 10;
+        tween.easing();
+        tween.on('repeat', (loopCount) => {
+          bgHole.scale.x -= 0.05;
+          bgHole.scale.y -= 0.05;
+        });
+        tween.start();
+
+        clicks++;
       }
+    }
+  };
 
-      backgroundTransparent.scale.x *= increment;
-      backgroundTransparent.scale.y *= increment;
+  const onJump = (event) => {
+    console.log(event);
+    console.log(jumpKey === event.keycode);
+    if (jumpKey === event.keycode) {
+      if (clicks > 0) {
+        console.log('SCALEEE');
 
-      clicks++;
+        const tween = PIXI.tweenManager.createTween(bgHole);
+        tween.time = 5;
+        tween.repeat = 10;
+        tween.easing();
+        tween.on('repeat', (loopCount) => {
+          bgHole.scale.x += 0.05;
+          bgHole.scale.y += 0.05;
+        });
+        tween.start();
+
+        clicks--;
+      }
     }
   };
 
   onClickSub = dixperPluginSample.onMouseDown$.subscribe(onClick);
+  onKeySub = dixperPluginSample.onKeyDown$.subscribe(onJump);
 };
