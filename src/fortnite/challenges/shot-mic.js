573cb48e059e332e1da91eee2a159d6d1087caa6
riiGalaxy <riimolpeceres@gmail.com>
2022-06-02 17:24:57 +0200

    sprite counter

--- src/fortnite/challenges/shot-mic.js
+++ src/fortnite/challenges/shot-mic.js
@@ -61,7 +61,7 @@ const init = () => {
 
   vumeter.onVolumeMatch = (volume) => {
     // shot();
-    console.log('onVolumeMatch', volume);
+    console.log("onVolumeMatch", volume);
     addFloatingText();
   };
 };
@@ -97,9 +97,9 @@ const createReminder = () => {
   setTimeout(() => {
     const reminder = new dxPanel(
       dixperPluginSample.pixi,
-      'reminder',
+      "reminder",
       dixperPluginSample.uiLayer,
-      'Salta por tu vida!!',
+      "Salta por tu vida!!",
       {
         position: {
           x: 200,
@@ -122,45 +122,45 @@ const lockShot = () => {
         ttl: 10000,
         actions: [
           {
-            inputKey: 'render-texture-0-0',
-            scope: '{{scope}}',
-            key: 'render-texture',
-            component: 'graphics',
-            type: 'render-texture',
+            inputKey: "render-texture-0-0",
+            scope: "{{scope}}",
+            key: "render-texture",
+            component: "graphics",
+            type: "render-texture",
             version: 1,
-            action: 'start',
+            action: "start",
             metadata: {
-              file: '{{file}}',
+              file: "{{file}}",
               textureProperties: {
-                width: '{{width}}',
-                height: '{{height}}',
-                position: '{{position}}',
-                fadeIn: '{{fade}}',
+                width: "{{width}}",
+                height: "{{height}}",
+                position: "{{position}}",
+                fadeIn: "{{fade}}",
               },
             },
-            tt0: '{{tt0}}',
-            ttl: '{{ttl}}',
+            tt0: "{{tt0}}",
+            ttl: "{{ttl}}",
           },
           {
-            inputKey: 'sound-0-1',
-            scope: '{{scope}}',
-            key: 'sound',
-            metadata: { file: '{{file}}', volume: '{{volume}}' },
-            tt0: '{{tt0}}',
-            ttl: '{{ttl}}',
+            inputKey: "sound-0-1",
+            scope: "{{scope}}",
+            key: "sound",
+            metadata: { file: "{{file}}", volume: "{{volume}}" },
+            tt0: "{{tt0}}",
+            ttl: "{{ttl}}",
           },
         ],
       },
     ]),
     {
-      'file||sound-0-1':
-        'https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FIUvnTvzg4RsRUwoll9pZ%2FAudio%20bicho%20cara%20fea.mp3?alt=media&token=a08c25ff-c138-4d2d-93f1-106106766ec0',
-      'ttl||sound-0-1': 10000,
-      'scope||sound-0-1': 100,
-      'file||render-texture-0-0':
-        'https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FX46ap915je4GhT9iGHLT%2Fassets%2Fsusto-ligth-1.png?alt=media&token=c8db59a9-6bd5-463f-99b7-0dead27aec3f',
-      'ttl||render-texture-0-0': 10000,
-      'scope||render-texture-0-0': 100,
+      "file||sound-0-1":
+        "https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FIUvnTvzg4RsRUwoll9pZ%2FAudio%20bicho%20cara%20fea.mp3?alt=media&token=a08c25ff-c138-4d2d-93f1-106106766ec0",
+      "ttl||sound-0-1": 10000,
+      "scope||sound-0-1": 100,
+      "file||render-texture-0-0":
+        "https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FX46ap915je4GhT9iGHLT%2Fassets%2Fsusto-ligth-1.png?alt=media&token=c8db59a9-6bd5-463f-99b7-0dead27aec3f",
+      "ttl||render-texture-0-0": 10000,
+      "scope||render-texture-0-0": 100,
     }
   );
 };
@@ -172,45 +172,45 @@ const shot = () => {
         ttl: 10000,
         actions: [
           {
-            inputKey: 'render-texture-0-0',
-            scope: '{{scope}}',
-            key: 'render-texture',
-            component: 'graphics',
-            type: 'render-texture',
+            inputKey: "render-texture-0-0",
+            scope: "{{scope}}",
+            key: "render-texture",
+            component: "graphics",
+            type: "render-texture",
             version: 1,
-            action: 'start',
+            action: "start",
             metadata: {
-              file: '{{file}}',
+              file: "{{file}}",
               textureProperties: {
-                width: '{{width}}',
-                height: '{{height}}',
-                position: '{{position}}',
-                fadeIn: '{{fade}}',
+                width: "{{width}}",
+                height: "{{height}}",
+                position: "{{position}}",
+                fadeIn: "{{fade}}",
               },
             },
-            tt0: '{{tt0}}',
-            ttl: '{{ttl}}',
+            tt0: "{{tt0}}",
+            ttl: "{{ttl}}",
           },
           {
-            inputKey: 'sound-0-1',
-            scope: '{{scope}}',
-            key: 'sound',
-            metadata: { file: '{{file}}', volume: '{{volume}}' },
-            tt0: '{{tt0}}',
-            ttl: '{{ttl}}',
+            inputKey: "sound-0-1",
+            scope: "{{scope}}",
+            key: "sound",
+            metadata: { file: "{{file}}", volume: "{{volume}}" },
+            tt0: "{{tt0}}",
+            ttl: "{{ttl}}",
           },
         ],
       },
     ]),
     {
-      'file||sound-0-1':
-        'https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FIUvnTvzg4RsRUwoll9pZ%2FAudio%20bicho%20cara%20fea.mp3?alt=media&token=a08c25ff-c138-4d2d-93f1-106106766ec0',
-      'ttl||sound-0-1': 10000,
-      'scope||sound-0-1': 100,
-      'file||render-texture-0-0':
-        'https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FX46ap915je4GhT9iGHLT%2Fassets%2Fsusto-ligth-1.png?alt=media&token=c8db59a9-6bd5-463f-99b7-0dead27aec3f',
-      'ttl||render-texture-0-0': 10000,
-      'scope||render-texture-0-0': 100,
+      "file||sound-0-1":
+        "https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FIUvnTvzg4RsRUwoll9pZ%2FAudio%20bicho%20cara%20fea.mp3?alt=media&token=a08c25ff-c138-4d2d-93f1-106106766ec0",
+      "ttl||sound-0-1": 10000,
+      "scope||sound-0-1": 100,
+      "file||render-texture-0-0":
+        "https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FX46ap915je4GhT9iGHLT%2Fassets%2Fsusto-ligth-1.png?alt=media&token=c8db59a9-6bd5-463f-99b7-0dead27aec3f",
+      "ttl||render-texture-0-0": 10000,
+      "scope||render-texture-0-0": 100,
     }
   );
 };
