925e3c619bdb9ddbb834e94f761db9bc4c6d37c9
riiGalaxy <riimolpeceres@gmail.com>
2022-06-07 13:14:13 +0200

    farts gogit add .!

--- src/fortnite/triggered/farts.js
+++ src/fortnite/triggered/farts.js
@@ -109,6 +109,7 @@ dixperPluginSample.onPixiLoad = () => {
   timer.onTimerStart = () => {};
 
   timer.onTimerFinish = () => {
+    clearSmoke();
     onClickSub.unsubscribe();
     onKeySub.unsubscribe();
   };
@@ -227,7 +228,6 @@ createFarts = () => {
   farts.onInFinish = () => {
     farts.remove();
   };
-  console.log("pedo", farts);
 };
 
 addSmoke = (level) => {
@@ -242,13 +242,12 @@ addSmoke = (level) => {
         x: DX_WIDTH / 2,
         y: DX_HEIGHT / 2,
       },
-      // size: {
-      //   width: DX_WIDTH,
-      //   height: DX_HEIGHT,
-      // },
+      size: {
+        width: DX_WIDTH,
+        height: DX_HEIGHT,
+      },
     }
   );
-  console.log("smoke", smoke);
 };
 
 clearSmoke = () => {
@@ -263,62 +262,10 @@ clearSmoke = () => {
         x: DX_WIDTH / 2,
         y: DX_HEIGHT / 2,
       },
-      // size: {
-      //   width: 1920,
-      //   height: 1080,
-      // },
+      size: {
+        width: 1920,
+        height: 1080,
+      },
     }
   );
-  console.log("clear", clear);
 };
-
-// //SUSTO
-// const sendJumpscare = () => {
-//   dixperPluginSample.addActions(
-//     JSON.stringify([
-//       {
-//         ttl: 10000,
-//         actions: [
-//           {
-//             inputKey: "render-texture-0-0",
-//             scope: "{{scope}}",
-//             key: "render-texture",
-//             component: "graphics",
-//             type: "render-texture",
-//             version: 1,
-//             action: "start",
-//             metadata: {
-//               file: "{{file}}",
-//               textureProperties: {
-//                 width: "{{width}}",
-//                 height: "{{height}}",
-//                 position: "{{position}}",
-//                 fadeIn: "{{fade}}",
-//               },
-//             },
-//             tt0: "{{tt0}}",
-//             ttl: "{{ttl}}",
-//           },
-//           {
-//             inputKey: "sound-0-1",
-//             scope: "{{scope}}",
-//             key: "sound",
-//             metadata: { file: "{{file}}", volume: "{{volume}}" },
-//             tt0: "{{tt0}}",
-//             ttl: "{{ttl}}",
-//           },
-//         ],
-//       },
-//     ]),
-//     {
-//       "file||sound-0-1":
-//         "https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FIUvnTvzg4RsRUwoll9pZ%2FAudio%20bicho%20cara%20fea.mp3?alt=media&token=a08c25ff-c138-4d2d-93f1-106106766ec0",
-//       "ttl||sound-0-1": 10000,
-//       "scope||sound-0-1": 100,
-//       "file||render-texture-0-0":
-//         "https://firebasestorage.googleapis.com/v0/b/dixper-abae2.appspot.com/o/skills%2FX46ap915je4GhT9iGHLT%2Fassets%2Fsusto-ligth-1.png?alt=media&token=c8db59a9-6bd5-463f-99b7-0dead27aec3f",
-//       "ttl||render-texture-0-0": 10000,
-//       "scope||render-texture-0-0": 100,
-//     }
-//   );
-// };
