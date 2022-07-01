6bb927e3468ec03377da300529d513175cefc7bd
Luis M Torres <lm.torresrivera@gmail.com>
2022-07-01 11:56:18 +0200

    [WIP]

--- src/common/reminder.js
+++ src/common/reminder.js
@@ -1,53 +1,46 @@
-function INIT() {
-  // INPUTS PARAMS
+// INPUTS PARAMS
 
-  let title, x, y;
+let title, x, y;
 
-  // DIXPER SDK INJECTED CLASS
+// DIXPER SDK INJECTED CLASS
 
-  const dixperPluginSample = new DixperSDKLib({
-    pixi: {
-      enable: true,
-      files: [],
-    },
-  });
+const dixperPluginSample = new DixperSDKLib({
+  pixi: {
+    enable: true,
+    files: [],
+  },
+});
 
-  // INPUTS
+// INPUTS
 
-  dixperPluginSample.inputs$.subscribe((inputs) => {
-    title = inputs.title || '';
-    x = inputs.x;
-    y = inputs.y;
-  });
+dixperPluginSample.inputs$.subscribe((inputs) => {
+  title = inputs.title || '';
+  x = inputs.x;
+  y = inputs.y;
+});
 
-  const transformRelativePosition = (x, y) => {
-    x = x ? (x * DX_WIDtH) / 100 : 200;
-    y = y ? (y * DX_HEIGHT) / 100 : DX_HEIGHT / 2 - 100;
+const transformRelativePosition = (x, y) => {
+  x = x ? (x * DX_WIDTH) / 100 : 200;
+  y = y ? (y * DX_HEIGHT) / 100 : DX_HEIGHT / 2 - 100;
 
-    return { x, y };
-  };
+  return { x, y };
+};
 
-  // PIXIJS INITILIZE
+// PIXIJS INITILIZE
 
-  dixperPluginSample.onPixiLoad = () => {
-    const reminder = new dxPanel(
-      dixperPluginSample.pixi,
-      'reminder',
-      dixperPluginSample.uiLayer,
-      title,
-      {
-        position: transformRelativePosition(x, y),
-        scale: {
-          x: 0.5,
-          y: 0.5,
-        },
-        animationSpeed: 0.5,
-      }
-    );
-  };
-}
-
-100 - DX_HEIGHT;
-2 - x;
-
-INIT();
+dixperPluginSample.onPixiLoad = () => {
+  const reminder = new dxPanel(
+    dixperPluginSample.pixi,
+    'reminder',
+    dixperPluginSample.uiLayer,
+    title,
+    {
+      position: transformRelativePosition(x, y),
+      scale: {
+        x: 0.5,
+        y: 0.5,
+      },
+      animationSpeed: 0.5,
+    }
+  );
+};
