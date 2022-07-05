1ac86a4b2e4976be26eb890752a34270937c73e3
riiGalaxy <riimolpeceres@gmail.com>
2022-07-05 12:03:16 +0200

    update

---
+++ src/fallguys/keyboard/triggered/farts-guys.js
@@ -0,0 +1,255 @@
+const images = [
+  {
+    name: "toxicBar",
+    url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/images/bar-toxic-progress-v2.png",
+  },
+];
+const sprites = [
+  {
+    name: "farts",
+    url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/spritesheets/farts.json",
+  },
+  {
+    name: "clearSmoke",
+    url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/spritesheets/clear-smoke.json",
+  },
+];
+const sounds = [
+  "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/sounds/farts/FART1.mp3",
+  "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/sounds/farts/FART2.mp3",
+  "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/sounds/farts/FART4.mp3",
+  "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/sounds/farts/FART5.mp3",
+  "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/sounds/farts/FART6.mp3",
+  "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/sounds/farts/FART7.mp3",
+];
+
+let onKeySub;
+let countClick = 0;
+let smoke;
+let alpha = 0;
+let randomSFX;
+
+// INPUTS PARAMS
+
+let reminderTitle,
+  actionKeys,
+  clickKeys,
+  maxFartSize,
+  minFartSize,
+  alphaIncrease,
+  alphaMax,
+  progressBar;
+
+// DIXPER SDK INJECTED CLASS
+
+const dixperPluginSample = new DixperSDKLib({
+  pixi: {
+    enable: true,
+    files: [...images, ...sprites, ...sounds],
+  },
+});
+
+// INPUTS
+
+dixperPluginSample.inputs$.subscribe((inputs) => {
+  actionKeys = inputs.actionKeys || [17, 29, 30, 31, 32, 42, 57];
+  alphaIncrease = inputs.alphaIncrease || 0.02;
+  alphaMax = inputs.alphaMax || 0.9;
+  reminderTitle = inputs.reminderTitle || "Que cene anoche???";
+  maxFartSize = inputs.maxFartSize || 0.25;
+  minFartSize = inputs.minFartSize || 0.1;
+});
+
+// PIXIJS INITILIZE
+
+dixperPluginSample.onPixiLoad = () => {
+  init();
+  createReminder();
+  const timestampUntilSkillFinish = dixperPluginSample.context.skillEnd;
+  const millisecondsToFinish = timestampUntilSkillFinish - Date.now() - 3000;
+  const interval = 1000;
+
+  const timer = new dxTimer(
+    dixperPluginSample.pixi,
+    "timer",
+    dixperPluginSample.uiLayer,
+    millisecondsToFinish,
+    interval,
+    {
+      position: {
+        x: (3 * DX_WIDTH) / 4,
+        y: 100,
+      },
+      animationSpeed: 0.3,
+      zIndex: 99,
+    }
+  );
+
+  timer.onTimerStart = () => {};
+
+  timer.onTimerFinish = () => {
+    clearSmoke();
+    onKeySub.unsubscribe();
+  };
+};
+
+const init = () => {
+  createSmoke();
+  createProgressBar();
+  createToxicBar();
+  onKeySub = dixperPluginSample.onKeyDown$.subscribe(onKeyboard);
+};
+
+const onKeyboard = (event) => {
+  // console.log("keycode", event.keycode);
+  if (actionKeys.includes(event.keycode) && !event.repeat) {
+    countClick++;
+    if (countClick % 1 === 0) {
+      createFarts(
+        Math.floor(
+          Math.random() * (DX_WIDTH / 2 + 40 - (DX_WIDTH / 2 - 40)) +
+            (DX_WIDTH / 2 - 40)
+        ),
+        Math.floor(
+          Math.random() * (DX_HEIGHT / 2 + 170 - (DX_HEIGHT / 2 + 70)) +
+            (DX_HEIGHT / 2 + 70)
+        ),
+        Math.random() * (maxFartSize - minFartSize) + minFartSize,
+        Math.floor(Math.random() * 6)
+      );
+    }
+    addSmoke(alphaIncrease);
+    createProgressBar();
+    createToxicBar();
+  }
+};
+
+function createFarts(posX, posY, size, randomSFX) {
+  let farts = new dxAnimatedElement(
+    dixperPluginSample.pixi,
+    "farts",
+    dixperPluginSample.uiLayer,
+    "",
+    {
+      animationSpeed: 0.5,
+      position: {
+        x: posX,
+        y: posY,
+      },
+      scale: {
+        x: size,
+        y: size,
+      },
+      zIndex: 99,
+    }
+  );
+  farts.onInFinish = () => {
+    farts._destroy();
+  };
+
+  const fartsSFX = PIXI.sound.Sound.from(sounds[randomSFX]);
+  fartsSFX.play({ volume: 0.5 });
+}
+
+function createSmoke() {
+  smoke = new PIXI.Graphics();
+  smoke.x = 0;
+  smoke.y = 0;
+  smoke.beginFill(0x1ecd4c, 0);
+  smoke.drawRect(0, 0, DX_WIDTH, DX_HEIGHT);
+  smoke.endFill();
+
+  dixperPluginSample.uiLayer.addChild(smoke);
+}
+
+function addSmoke(alphaParam) {
+  if (alpha < alphaMax) {
+    alpha += alphaParam;
+    smoke.clear();
+    smoke.beginFill(0x1ecd4c, alpha);
+    smoke.drawRect(0, 0, DX_WIDTH, DX_HEIGHT);
+    smoke.endFill();
+  }
+}
+
+function clearSmoke() {
+  let clear = new dxAnimatedElement(
+    dixperPluginSample.pixi,
+    "clearSmoke",
+    dixperPluginSample.uiLayer,
+    "",
+    {
+      animationSpeed: 0.5,
+      position: {
+        x: DX_WIDTH / 2,
+        y: DX_HEIGHT / 2,
+      },
+      scale: {
+        x: 1.5,
+        y: 1.5,
+      },
+    }
+  );
+}
+
+function createProgressBar() {
+  let progress = 105;
+  //min 105 max 480
+  const conversionNumber = 416;
+  progress += alpha * conversionNumber;
+  const DX = DX_WIDTH / 2 - 256;
+  const DY = 57;
+
+  const coordinates = [
+    DX + 69,
+    DY + 21,
+    DX + 93,
+    DY + 11,
+    DX + progress,
+    DY + 11,
+    DX + progress,
+    DY + 65,
+    DX + 105,
+    DY + 63,
+    DX + 70,
+    DY + 21,
+  ];
+  progressBar = new PIXI.Graphics();
+  progressBar.clear();
+  progressBar.beginFill(0xea4e69);
+  progressBar.drawPolygon(coordinates);
+  progressBar.endFill();
+
+  dixperPluginSample.uiLayer.addChild(progressBar);
+}
+
+function createToxicBar() {
+  const toxicBar = new PIXI.Sprite.from(
+    dixperPluginSample.pixi.resources.toxicBar.texture
+  );
+  toxicBar.x = DX_WIDTH / 2;
+  toxicBar.y = 100;
+  toxicBar.anchor.set(0.5);
+  toxicBar.zIndex = 99;
+
+  dixperPluginSample.uiLayer.addChild(toxicBar);
+}
+function createReminder() {
+  const reminder = new dxPanel(
+    dixperPluginSample.pixi,
+    "reminder",
+    dixperPluginSample.uiLayer,
+    reminderTitle,
+    {
+      position: {
+        x: 200,
+        y: DX_HEIGHT / 2 - 100,
+      },
+      scale: {
+        x: 0.5,
+        y: 0.5,
+      },
+      animationSpeed: 0.5,
+    }
+  );
+}
