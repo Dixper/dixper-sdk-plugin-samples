1f8d2e630b37e1f3900cbf5e473b9b3b7323a113
Luis M Torres <lm.torresrivera@gmail.com>
2022-07-08 10:20:29 +0200

    [WIP]

--- src/common/reminder.js
+++ src/common/reminder.js
@@ -1,9 +1,4 @@
-// INPUTS PARAMS
-
-let title, x, y;
-
 // DIXPER SDK INJECTED CLASS
-
 const dixperPluginSample = new DixperSDKLib({
   pixi: {
     enable: true,
@@ -12,35 +7,13 @@ const dixperPluginSample = new DixperSDKLib({
 });
 
 // INPUTS
+const { title, x, y } = DX_INPUTS;
 
-dixperPluginSample.inputs$.subscribe((inputs) => {
-  title = inputs.title || '';
-  x = inputs.x;
-  y = inputs.y;
+const reminder = new dxPanel(DX_PIXI, 'reminder', DX_LAYERS.ui, title, {
+  position: DX_UTILS.transformRelativePosition(x, y),
+  scale: {
+    x: 0.5,
+    y: 0.5,
+  },
+  animationSpeed: 0.5,
 });
-
-const transformRelativePosition = (x, y) => {
-  x = x ? (x * DX_WIDTH) / 100 : 200;
-  y = y ? (y * DX_HEIGHT) / 100 : DX_HEIGHT / 2 - 100;
-
-  return { x, y };
-};
-
-// PIXIJS INITILIZE
-
-dixperPluginSample.onPixiLoad = () => {
-  const reminder = new dxPanel(
-    dixperPluginSample.pixi,
-    'reminder',
-    dixperPluginSample.uiLayer,
-    title,
-    {
-      position: transformRelativePosition(x, y),
-      scale: {
-        x: 0.5,
-        y: 0.5,
-      },
-      animationSpeed: 0.5,
-    }
-  );
-};
---
+++ src/fallguys/gamepad/triggered/broke-screen-jump.js
@@ -0,0 +1,81 @@
+const images = [];
+const sprites = [];
+const sounds = [];
+
+let onKeySub;
+let actionType = 'start';
+let fadeIncrement = 0.1;
+let shadowIncrement = 0.1;
+
+// DIXPER SDK INJECTED CLASS
+
+const dixperPluginSample = new DixperSDKLib({
+  pixi: {
+    enable: true,
+    files: [...images, ...sprites, ...sounds],
+  },
+});
+
+// INPUTS
+
+dixperPluginSample.inputs$.subscribe((inputs) => {});
+
+// PIXIJS INITILIZE
+
+dixperPluginSample.onPixiLoad = () => {
+  init();
+};
+
+const init = () => {
+  onKeySub = dixperPluginSample.onKeyDown$.subscribe(onKeyboard);
+};
+
+const onKeyboard = (event) => {
+  console.log('keycode', event.keycode);
+  if (event.keycode === 57 && !event.repeat) {
+    // const jumpSFX = PIXI.sound.Sound.from(sounds[1]);
+    // jumpSFX.play({ volume: 0.5 });
+    applyEffect();
+  }
+};
+
+const applyEffect = () => {
+  const timestampUntilSkillFinish = dixperPluginSample.context.skillEnd;
+  const millisecondsToFinish = timestampUntilSkillFinish - Date.now();
+
+  dixperPluginSample.addActions(
+    JSON.stringify([
+      {
+        actions: [
+          {
+            inputKey: 'imagepostprocessing||1',
+            scope: '{{scope}}',
+            key: 'imagePostProcessing',
+            component: 'graphicsPostProcessor',
+            type: 'imagePostProcessing',
+            action: actionType,
+            metadata: {
+              effect: '{{effect}}',
+              effectData: '{{effectdata}}',
+            },
+            tt0: '{{tt0}}',
+            ttl: '{{ttl}}',
+          },
+        ],
+        ttl: 0,
+      },
+    ]),
+    {
+      'scope||imagepostprocessing||1': [0],
+      'effect||imagepostprocessing||1': 'CameraFilterPack_Broken_Screen',
+      'effectdata||imagepostprocessing||1': `{"Fade": ${fadeIncrement},"Shadow": ${shadowIncrement}}`,
+      'tt0||imagepostprocessing||1': 0,
+      'ttl||imagepostprocessing||1': millisecondsToFinish,
+    }
+  );
+
+  shadowIncrement += 0.1;
+  fadeIncrement += 0.1;
+
+  actionType = 'update';
+};
