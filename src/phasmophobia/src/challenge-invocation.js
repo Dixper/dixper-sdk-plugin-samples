cf47fff4ac6a0b7ebd7901e582a1a47904ef70e5
riiGalaxy <riimolpeceres@gmail.com>
2022-08-16 12:56:41 +0200

    assets [wip]

---
+++ src/phasmophobia/src/challenge-invocation.js
@@ -0,0 +1,116 @@
+const images = [];
+const sprites = [
+  {
+    name: "drawButton",
+    url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/main/src/fortnite/assets/spritesheets/button.json",
+  },
+];
+const sounds = [];
+
+// INPUTS PARAMS
+
+let reminder, onKeySub, onClickSub, firstButton;
+
+// DIXPER SDK INJECTED CLASS
+
+const dixperPluginSample = new DixperSDKLib({
+  pixi: {
+    enable: true,
+    files: [...images, ...sprites, ...sounds],
+  },
+});
+
+// INPUTS
+
+const { challengeTitle, challengeTime, reminderTitle } = DX_INPUTS;
+
+// PIXIJS INITILIZE
+
+dixperPluginSample.onPixiLoad = () => {
+  dixperPluginSample.initChallenge(challengeTitle, challengeTime);
+};
+
+// INIT CHALLENGE
+
+dixperPluginSample.onChallengeAccepted = () => {
+  init();
+};
+
+dixperPluginSample.onChallengeRejected = () => {
+  dixperPluginSample.stopSkill();
+};
+
+dixperPluginSample.onChallengeFinish = () => {
+  // if (reminder) {
+  //   dixperPluginSample.challengeSuccess();
+  //   reminder.remove();
+  //   setTimeout(() => dixperPluginSample.stopSkill(), 3000);
+  // } else {
+  //   dixperPluginSample.challengeFail();
+  //   reminder.remove();
+  //   setTimeout(
+  //     () => dixperPluginSample.addParentSkill("zmwKfnd7vzV7HZ07uK3s"),
+  //     2000
+  //   );
+  // }
+};
+
+const init = () => {
+  console.log("init");
+  // if (DX_CONTROLLER_TYPE) {
+  //   onKeySub = dixperPluginSample.onGamepadButtonPress$.subscribe(onGamepad);
+  // } else {
+  //   onKeySub = dixperPluginSample.onKeyDown$.subscribe(addFloatingText);
+  // }
+  onClickSub = dixperPluginSample.onMouseDown$.subscribe(createButtonOptions);
+  createReminder();
+  // createTokenInvocation();
+  createButtonOptions();
+};
+
+const createReminder = () => {
+  reminder = new dxPanel(DX_PIXI, "reminder", DX_LAYERS.ui, reminderTitle, {
+    position: {
+      x: 200,
+      y: DX_HEIGHT / 2 - 100,
+    },
+    scale: {
+      x: 0.5,
+      y: 0.5,
+    },
+    animationSpeed: 0.5,
+  });
+};
+
+// const createTokenInvocation = () => {
+//   console.log("createToken");
+//   const graphics = new PIXI.Graphics();
+//   const path = [600, 370, 700, 460, 780, 420, 730, 570, 590, 520];
+
+//   graphics.lineStyle(0);
+//   graphics.beginFill(0x3500fa, 1);
+//   graphics.drawPolygon(path);
+//   graphics.endFill();
+
+//   DX_LAYERS.ui.addChild(graphics);
+// };
+
+const createButtonOptions = () => {
+  const createFirstButton = () => {
+    firstButton = new dxButton(DX_PIXI, "drawButton", DX_LAYERS.ui, "", {
+      position: {
+        x: DX_WIDTH / 2 - 250,
+        y: DX_HEIGHT / 2,
+      },
+      scale: {
+        x: 0.25,
+        y: 0.25,
+      },
+      animationSpeed: 0.5,
+      hitbox: [-175, -45, 175, -45, 175, 45, -175, 46],
+    });
+    return firstButton;
+  };
+  createFirstButton();
+  console.log("firstButton", firstButton);
+};
