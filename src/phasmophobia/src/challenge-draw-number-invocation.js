52b96f731fbcc32f68139a9b35b1996d9ccccec8
riiGalaxy <riimolpeceres@gmail.com>
2022-08-31 15:29:16 +0200

    update sprites

--- src/phasmophobia/src/challenge-draw-number-invocation.js
+++ src/phasmophobia/src/challenge-draw-number-invocation.js
@@ -7,6 +7,10 @@ const images = [
     name: "drawClick",
     url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/phasmophobia/src/phasmophobia/assets/images/draw_button_invocation_filled.png",
   },
+  {
+    name: "firstDrawClick",
+    url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/phasmophobia/src/phasmophobia/assets/images/first_draw_button_invocation_filled.png",
+  },
 ];
 const sprites = [
   // {
@@ -24,16 +28,20 @@ const sprites = [
 ];
 const sounds = [];
 
-let reminder, randomPosition, randomSymbol, randomOrder, x, y, onClickSub;
-let firstButtonClicked,
-  secondButtonClicked,
-  firstCirclePainted,
-  secondCirclePainted;
+let reminder,
+  randomPosition,
+  randomSymbol,
+  randomOrder,
+  x,
+  y,
+  onClickSub,
+  button;
 let buttonsArray = [];
 let previousPoint;
 let currentPoint;
-let currentCirclePainted;
 let previousCirclePainted;
+let randomStart;
+let newSymbol;
 let circlePainted;
 let jsonArray = [];
 let idForCoordenates = 0;
@@ -150,105 +158,90 @@ const buttonSettingsSymbol_2 = [
     y: 459,
     id: 2,
     connections: [4, 5, 7, 8],
-    drawLine: false,
   },
   {
     x: 969,
     y: 218,
     id: 1,
     connections: [3, 4, 6, 15],
-    drawLine: false,
   },
   {
     x: 741,
     y: 830,
     id: 3,
     connections: [1, 5, 9, 10],
-    drawLine: false,
   },
   {
     x: 1327,
     y: 454,
     id: 5,
     connections: [2, 3, 13, 14],
-    drawLine: false,
   },
   {
     x: 782,
     y: 256,
     id: 6,
     connections: [1, 7],
-    drawLine: false,
   },
   {
     x: 671,
     y: 335,
     id: 7,
     connections: [2, 6],
-    drawLine: false,
   },
   {
     x: 1184,
     y: 824,
     id: 4,
     connections: [1, 2, 11, 12],
-    drawLine: false,
   },
   {
     x: 585,
     y: 586,
     id: 8,
     connections: [2, 9],
-    drawLine: false,
   },
   {
     x: 638,
     y: 728,
     id: 9,
     connections: [3, 8],
-    drawLine: false,
   },
   {
     x: 876,
     y: 887,
     id: 10,
     connections: [3, 11],
-    drawLine: false,
   },
   {
     x: 1086,
     y: 881,
     id: 11,
     connections: [4, 10],
-    drawLine: false,
   },
   {
     x: 1285,
     y: 741,
     id: 12,
     connections: [4, 13],
-    drawLine: false,
   },
   {
     x: 1340,
     y: 630,
     id: 13,
     connections: [5, 12],
-    drawLine: false,
   },
   {
     x: 1266,
     y: 344,
     id: 14,
     connections: [5, 15],
-    drawLine: false,
   },
   {
     x: 1120,
     y: 247,
     id: 15,
     connections: [1, 14],
-    drawLine: false,
   },
 ];
 const buttonSettingsSymbol_3 = [
@@ -257,84 +250,72 @@ const buttonSettingsSymbol_3 = [
     y: 234,
     id: 1,
     connections: [2],
-    drawLine: false,
   },
   {
     x: 975,
     y: 733,
     id: 2,
     connections: [1, 7, 9, 10, 12],
-    drawLine: false,
   },
   {
     x: 756,
     y: 347,
     id: 3,
     connections: [4],
-    drawLine: false,
   },
   {
     x: 1194,
     y: 344,
     id: 4,
     connections: [3],
-    drawLine: false,
   },
   {
     x: 699,
     y: 492,
     id: 5,
     connections: [6],
-    drawLine: false,
   },
   {
     x: 1244,
     y: 489,
     id: 6,
     connections: [5],
-    drawLine: false,
   },
   {
     x: 1201,
     y: 616,
     id: 7,
     connections: [2, 8],
-    drawLine: false,
   },
   {
     x: 1366,
     y: 733,
     id: 8,
     connections: [7, 9],
-    drawLine: false,
   },
   {
     x: 1223,
     y: 855,
     id: 9,
     connections: [2, 8],
-    drawLine: false,
   },
   {
     x: 731,
     y: 617,
     id: 10,
     connections: [2, 11],
-    drawLine: false,
   },
   {
     x: 581,
     y: 726,
     id: 11,
     connections: [10, 12],
-    drawLine: false,
   },
   {
     x: 738,
     y: 857,
     id: 12,
     connections: [2, 11],
-    drawLine: false,
   },
 ];
 const buttonSettingsSymbol_4 = [
@@ -343,77 +324,66 @@ const buttonSettingsSymbol_4 = [
     y: 182,
     id: 1,
     connections: [2, 4, 6],
-    drawLine: false,
   },
   {
     x: 1266,
     y: 187,
     id: 2,
     connections: [1, 3, 5],
-    drawLine: false,
   },
   {
     x: 690,
     y: 717,
     id: 3,
     connections: [2],
-    drawLine: false,
   },
   {
     x: 1154,
     y: 715,
     id: 4,
     connections: [1],
-    drawLine: false,
   },
   {
     x: 764,
     y: 944,
     id: 5,
     connections: [2, 8],
-    drawLine: false,
   },
   {
     x: 1108,
     y: 948,
     id: 6,
     connections: [1, 7],
-    drawLine: false,
   },
   {
     x: 1155,
     y: 869,
     id: 7,
     connections: [6],
-    drawLine: false,
   },
   {
     x: 681,
     y: 868,
     id: 8,
     connections: [5],
-    drawLine: false,
   },
   {
     x: 1032,
     y: 807,
     id: 9,
     connections: [11],
-    drawLine: false,
   },
   {
     x: 812,
     y: 808,
     id: 10,
     connections: [11],
-    drawLine: false,
   },
   {
     x: 923,
     y: 984,
     id: 11,
     connections: [9, 10],
-    drawLine: false,
   },
 ];
 const buttonSettingsSymbol_5 = [
@@ -422,217 +392,186 @@ const buttonSettingsSymbol_5 = [
     y: 317,
     id: 1,
     connections: [2],
-    drawLine: false,
   },
   {
     x: 1038,
     y: 230,
     id: 2,
     connections: [1, 3],
-    drawLine: false,
   },
   {
     x: 938,
     y: 326,
     id: 3,
     connections: [2, 4],
-    drawLine: false,
   },
   {
     x: 895,
     y: 473,
     id: 4,
     connections: [3],
-    drawLine: false,
   },
   {
     x: 851,
     y: 393,
     id: 5,
     connections: [6],
-    drawLine: false,
   },
   {
     x: 729,
     y: 595,
     id: 6,
     connections: [5, 7],
-    drawLine: false,
   },
   {
     x: 811,
     y: 750,
     id: 7,
     connections: [6],
-    drawLine: false,
   },
   {
     x: 692,
     y: 673,
     id: 8,
     connections: [9],
-    drawLine: false,
   },
   {
     x: 640,
     y: 778,
     id: 9,
     connections: [8, 10],
-    drawLine: false,
   },
   {
     x: 805,
     y: 804,
     id: 10,
     connections: [9, 11],
-    drawLine: false,
   },
   {
     x: 990,
     y: 759,
     id: 11,
     connections: [10],
-    drawLine: false,
   },
   {
     x: 797,
     y: 577,
     id: 12,
     connections: [13],
-    drawLine: false,
   },
   {
     x: 955,
     y: 508,
     id: 13,
     connections: [12, 14],
-    drawLine: false,
   },
   {
     x: 1127,
     y: 514,
     id: 14,
     connections: [13],
-    drawLine: false,
   },
   {
     x: 911,
     y: 576,
     id: 15,
     connections: [16],
-    drawLine: false,
   },
   {
     x: 989,
     y: 696,
     id: 16,
     connections: [15, 17],
-    drawLine: false,
   },
   {
     x: 1142,
     y: 769,
     id: 17,
     connections: [16],
-    drawLine: false,
   },
   {
     x: 929,
     y: 830,
     id: 18,
     connections: [19],
-    drawLine: false,
   },
   {
     x: 1186,
     y: 820,
     id: 19,
     connections: [18, 20],
-    drawLine: false,
   },
   {
     x: 1318,
     y: 681,
     id: 20,
     connections: [19],
-    drawLine: false,
   },
   {
     x: 1090,
     y: 696,
     id: 21,
     connections: [22],
-    drawLine: false,
   },
   {
     x: 1172,
     y: 590,
     id: 22,
     connections: [21, 23],
-    drawLine: false,
   },
   {
     x: 1185,
     y: 445,
     id: 23,
     connections: [22],
-    drawLine: false,
   },
   {
     x: 1243,
     y: 551,
     id: 24,
     connections: [25],
-    drawLine: false,
   },
   {
     x: 1377,
     y: 650,
     id: 25,
     connections: [24, 26],
-    drawLine: false,
   },
   {
     x: 1436,
     y: 780,
     id: 26,
     connections: [25, 27],
-    drawLine: false,
   },
   {
     x: 1305,
     y: 803,
     id: 27,
     connections: [26],
-    drawLine: false,
   },
   {
     x: 1349,
     y: 550,
     id: 28,
     connections: [29],
-    drawLine: false,
   },
   {
     x: 1295,
     y: 444,
     id: 29,
     connections: [28, 30],
-    drawLine: false,
   },
   {
     x: 1200,
     y: 383,
     id: 30,
     connections: [29, 31],
-    drawLine: false,
   },
   {
     x: 985,
     y: 358,
     id: 31,
     connections: [30],
-    drawLine: false,
   },
 ];
 const buttonSettingsSymbol_6 = [
@@ -739,9 +678,17 @@ const init = () => {
   // console.log("init");
   createReminder();
   createRandomSymbol();
-  createButtons(randomOrder);
+
+  //random Order -----------------------
+  // createButtons(randomOrder);
+
+  //order button ------------------
   // createButtons(randomSymbol);
-  onClickSub = dixperPluginSample.onMouseDown$.subscribe(onKeyOrClick);
+
+  // change first button order
+  createButtons(newSymbol);
+
+  // onClickSub = dixperPluginSample.onMouseDown$.subscribe(onKeyOrClick);
 };
 
 /*
@@ -750,10 +697,9 @@ CREATE INIT FUNCTIONS - START
 const createRandomSymbol = () => {
   randomPosition = Math.floor(Math.random() * symbolsOfInvocation.length);
   randomSymbol = [...symbolsOfInvocation[randomPosition]];
-  // console.log("randomPosition", randomPosition);
-  // console.log("randomSymbol", randomSymbol);
   // randomSymbol = [...symbolsOfInvocation[6]];
-  createRandomOrder(randomSymbol);
+  // createRandomOrder(randomSymbol);
+  createRandomStart(randomSymbol);
 };
 
 const createRandomOrder = () => {
@@ -763,32 +709,62 @@ const createRandomOrder = () => {
     .map(({ value }) => value);
 };
 
+const createRandomStart = () => {
+  randomStart = Math.floor(Math.random() * randomSymbol.length);
+
+  const newStartSymbol = randomSymbol.splice(randomStart);
+
+  newSymbol = newStartSymbol.concat(randomSymbol);
+};
+
 const createButtons = (symbolSelected) => {
   symbolSelected.forEach((current, index) => {
-    const button = new dxButton(
-      DX_PIXI,
-      "drawButton",
-      DX_LAYERS.ui,
-      `${index + 1}`,
-      {
-        position: {
-          x: current.x,
-          y: current.y,
-        },
-        scale: {
-          x: 0.2,
-          y: 0.2,
-        },
-        animationSpeed: 0.5,
-        text: {
-          fontSize: 130,
-        },
-      }
-    );
+    if (index === 0) {
+      button = new dxButton(
+        DX_PIXI,
+        "firstDrawButton",
+        DX_LAYERS.ui,
+        `${index + 1}`,
+        {
+          position: {
+            x: current.x,
+            y: current.y,
+          },
+          scale: {
+            x: 0.2,
+            y: 0.2,
+          },
+          animationSpeed: 0.5,
+          text: {
+            fontSize: 130,
+          },
+        }
+      );
+    } else {
+      button = new dxButton(
+        DX_PIXI,
+        "drawButton",
+        DX_LAYERS.ui,
+        `${index + 1}`,
+        {
+          position: {
+            x: current.x,
+            y: current.y,
+          },
+          scale: {
+            x: 0.2,
+            y: 0.2,
+          },
+          animationSpeed: 0.5,
+          text: {
+            fontSize: 130,
+          },
+        }
+      );
+    }
 
     buttonsArray.push({
       clicked: false,
-      drawLine: current.drawLine,
       instance: button,
       id: current.id,
       connections: current.connections,
@@ -822,8 +798,8 @@ CREATE INIT FUNCTIONS - END
 const createLine = (path) => {
   // console.log("createLine");
   const line = new PIXI.Graphics();
-  line.lineStyle(5);
-  line.beginFill(0x650a5a, 1);
+  line.lineStyle(10);
+  line.beginFill(0x650a5a, 0.9);
   line.drawPolygon(path);
   DX_LAYERS.ui.addChild(line);
 };
@@ -839,26 +815,39 @@ const createPointDraw = (x, y) => {
   return pointDraw;
 };
 
-const onKeyOrClick = (event) => {
-  // console.log("event", event);
-  idForCoordenates += 1;
-  let coordenatesSymbol = {
-    x: event.x,
-    y: event.y,
-    id: idForCoordenates,
-    connections: [],
-  };
-  jsonArray.push(coordenatesSymbol);
-  let JsonObject = JSON.stringify(jsonArray);
-  console.log("Objeto nuevo", JsonObject);
+const createFirstPointDraw = (x, y) => {
+  // console.log("createPointDraw");
+  const pointDraw = new PIXI.Sprite.from(
+    DX_PIXI.resources.firstDrawClick.texture
+  );
+  pointDraw.x = x;
+  pointDraw.y = y;
+  pointDraw.scale = { x: 0.1, y: 0.1 };
+  pointDraw.anchor = { x: 0.5, y: 0.5 };
+
+  return pointDraw;
 };
+
+// const onKeyOrClick = (event) => {
+// console.log("event", event);
+// idForCoordenates += 1;
+// let coordenatesSymbol = {
+//   x: event.x,
+//   y: event.y,
+//   id: idForCoordenates,
+//   connections: [],
+// };
+// jsonArray.push(coordenatesSymbol);
+// let JsonObject = JSON.stringify(jsonArray);
+// console.log("Objeto nuevo", JsonObject);
+// };
 
 const checkClickButton = (buttonInstance) => {
   buttonInstance.instance.onClick = () => {
     if (buttonInstance.index === 0 && buttonInstance.clicked === false) {
       previousPoint = buttonInstance;
       previousPoint.clicked = true;
-      previousCirclePainted = createPointDraw(
+      previousCirclePainted = createFirstPointDraw(
         previousPoint.instance._options.position.x,
         previousPoint.instance._options.position.y
       );
---
+++ src/phasmophobia/src/challenge-quiz.js
@@ -0,0 +1,257 @@
+const images = [];
+const sprites = [
+  {
+    name: "ghostPanel",
+    url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/phasmophobia/src/phasmophobia/assets/spritesheets/button_draw_invocation.json",
+  },
+];
+const sounds = [];
+
+let reminder, ghost;
+
+// INPUTS PARAMS
+
+const ghostsList = [
+  {
+    ghost: "Banshee",
+    ghost_id: 1,
+    evidences: [2, 4, 6],
+  },
+  {
+    ghost: "Demon",
+    ghost_id: 2,
+    evidences: [1, 6, 7],
+  },
+  {
+    ghost: "Deogen",
+    ghost_id: 3,
+    evidences: [3, 4, 7],
+  },
+  {
+    ghost: "Goryo",
+    ghost_id: 4,
+    evidences: [4, 5, 6],
+  },
+  {
+    ghost: "Hantu",
+    ghost_id: 5,
+    evidences: [1, 2, 6],
+  },
+  {
+    ghost: "Jinn",
+    ghost_id: 6,
+    evidences: [1, 5, 6],
+  },
+  {
+    ghost: "Mare",
+    ghost_id: 7,
+    evidences: [2, 3, 7],
+  },
+  {
+    ghost: "Moroi",
+    ghost_id: 8,
+    evidences: [1, 3, 7],
+  },
+  {
+    ghost: "Myling",
+    ghost_id: 9,
+    evidences: [5, 6, 7],
+  },
+  {
+    ghost: "Obake",
+    ghost_id: 10,
+    evidences: [2, 5, 6],
+  },
+  {
+    ghost: "Oni",
+    ghost_id: 11,
+    evidences: [1, 4, 5],
+  },
+  {
+    ghost: "Onryo",
+    ghost_id: 12,
+    evidences: [1, 2, 3],
+  },
+  {
+    ghost: "Phantom",
+    ghost_id: 13,
+    evidences: [3, 4, 6],
+  },
+  {
+    ghost: "Poltergeist",
+    ghost_id: 14,
+    evidences: [3, 6, 7],
+  },
+  {
+    ghost: "Raiju",
+    ghost_id: 15,
+    evidences: [2, 4, 5],
+  },
+  {
+    ghost: "Revenant",
+    ghost_id: 16,
+    evidences: [1, 2, 7],
+  },
+  {
+    ghost: "Shade",
+    ghost_id: 17,
+    evidences: [1, 5, 7],
+  },
+  {
+    ghost: "Spirit",
+    ghost_id: 18,
+    evidences: [3, 5, 7],
+  },
+  {
+    ghost: "Thaye",
+    ghost_id: 19,
+    evidences: [2, 4, 7],
+  },
+  {
+    ghost: "The Mimic",
+    ghost_id: 20,
+    evidences: [1, 2, 3, 6],
+  },
+  {
+    ghost: "The Twins",
+    ghost_id: 21,
+    evidences: [1, 3, 5],
+  },
+  {
+    ghost: "Wraith",
+    ghost_id: 22,
+    evidences: [3, 4, 5],
+  },
+  {
+    ghost: "Yokai",
+    ghost_id: 23,
+    evidences: [2, 3, 4],
+  },
+  {
+    ghost: "Yurei",
+    ghost_id: 24,
+    evidences: [1, 2, 4],
+  },
+];
+
+const evidencesList = [
+  {
+    evidence: "Freezing Temperatures",
+    evidence_id: 1,
+    ghosts: [2, 5, 6, 8, 11, 12, 16, 17, 20, 21, 24],
+  },
+  {
+    evidence: "Ghost Orb",
+    evidence_id: 2,
+    ghosts: [1, 5, 7, 10, 12, 15, 16, 19, 20, 23, 24],
+  },
+  {
+    evidence: "Spirit Box",
+    evidence_id: 3,
+    ghosts: [3, 7, 8, 12, 13, 14, 18, 20, 21, 22, 23],
+  },
+  {
+    evidence: "DOTS Projector",
+    evidence_id: 4,
+    ghosts: [1, 3, 4, 11, 13, 15, 19, 22, 23, 24],
+  },
+  {
+    evidence: "EMF Level 5",
+    evidence_id: 5,
+    ghosts: [4, 6, 9, 10, 11, 15, 17, 18, 21, 22],
+  },
+  {
+    evidence: "Fingerprints",
+    evidence_id: 6,
+    ghosts: [1, 2, 4, 5, 6, 9, 10, 13, 14, 20],
+  },
+  {
+    evidence: "Ghost Writing",
+    evidence_id: 7,
+    ghosts: [2, 3, 7, 8, 9, 14, 16, 17, 18, 19],
+  },
+];
+
+// DIXPER SDK INJECTED CLASS
+
+const dixperPluginSample = new DixperSDKLib({
+  pixi: {
+    enable: true,
+    files: [...images, ...sprites, ...sounds],
+  },
+});
+
+// INPUTS
+
+const { challengeTitle, challengeTime, reminderTitle } = DX_INPUTS;
+
+// PIXIJS INITILIZE
+
+dixperPluginSample.onPixiLoad = () => {
+  dixperPluginSample.initChallenge(challengeTitle, challengeTime);
+};
+
+// INIT CHALLENGE
+
+dixperPluginSample.onChallengeAccepted = () => {
+  init();
+};
+
+dixperPluginSample.onChallengeRejected = () => {
+  dixperPluginSample.stopSkill();
+};
+
+dixperPluginSample.onChallengeFinish = () => {
+  console.log("onChallengeFinish");
+  if (reminder) {
+    reminder.remove();
+  }
+  dixperPluginSample.challengeFail();
+  dixperPluginSample.stopSkill();
+};
+const init = () => {
+  console.log("init");
+  createReminder();
+  createGhost();
+  // onClickSub = dixperPluginSample.onMouseDown$.subscribe(onKeyOrClick);
+};
+
+/*
+CREATE INIT FUNCTIONS - START
+*/
+
+const createReminder = () => {
+  reminder = new dxPanel(DX_PIXI, "reminder", DX_LAYERS.ui, reminderTitle, {
+    position: {
+      x: 200,
+      y: DX_HEIGHT / 2 - 100,
+    },
+    scale: {
+      x: 0.5,
+      y: 0.5,
+    },
+    animationSpeed: 0.5,
+  });
+};
+
+const createGhost = () => {
+  ghost = new dxPanel(DX_PIXI, "ghostPanel", DX_LAYERS.ui, reminderTitle, {
+    position: {
+      x: DX_WIDTH / 2,
+      y: DX_HEIGHT / 2 - 100,
+    },
+    scale: {
+      x: 0.5,
+      y: 0.5,
+    },
+    animationSpeed: 0.5,
+  });
+};
+
+/*
+CREATE INIT FUNCTIONS - END
+*/
+
+const onKeyOrClick = (event) => {
+  console.log("event", event);
+};
