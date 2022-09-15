66f9407efbb84a701d63502031b48b234f2fc989
adriagonzalez <adria.gonzalez@dixper.gg>
2022-09-15 09:40:46 +0200

    starting tarot card skill

---
+++ src/halloween/src/base-script.js
@@ -0,0 +1,49 @@
+const images = [];
+
+const sprites = [];
+
+const sounds = [];
+
+
+// INPUTS PARAMS
+
+
+
+// DIXPER SDK INJECTED CLASS
+
+const dixperPluginSample = new DixperSDKLib({
+    pixi: {
+        enable: true,
+        files: [...images, ...sprites, ...sounds],
+    },
+});
+
+// INPUTS
+
+const { } =
+    DX_INPUTS;
+
+// PIXIJS INITILIZE
+
+dixperPluginSample.onPixiLoad = () => {
+    init();
+    //dixperPluginSample.initChallenge(challengeTitle, challengeTime);
+};
+
+// INIT CHALLENGE
+
+dixperPluginSample.onChallengeAccepted = () => {
+    init();
+};
+
+dixperPluginSample.onChallengeRejected = () => {
+    dixperPluginSample.stopSkill();
+};
+
+dixperPluginSample.onChallengeFinish = () => {
+};
+
+const init = () => {
+
+}
+
