67561e93d8934470e0f677075c78a0cda0ef218a
adriagonzalez <adria.gonzalez@dixper.gg>
2022-09-16 13:30:41 +0200

    wip tarot cards

--- src/halloween/src/tarot-card.js
+++ src/halloween/src/tarot-card.js
@@ -1,26 +1,36 @@
 const images = [
     {
         name: "tarotFront1",
-        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-front-1.webp"
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/spritesheets/tarot-front-1.png"
     },
     {
-        name: "tarotBack",
-        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-back.png"
+        name: "tarotFront2",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/spritesheets/tarot-front-2.png"
     },
+    {
+        name: "tarotFront3",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/spritesheets/tarot-front-3.png"
+    },
+];
+
+const sprites = [
+    {
+        name: "tarotBack",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/spritesheets/tarot-back.json"
+    },
+
     {
         name: "invisibleButton",
         url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/phasmophobia/src/phasmophobia/assets/spritesheets/invisible_sprite.json",
     }
 ];
 
-const sprites = [];
-
 const sounds = [];
 
 
 // INPUTS PARAMS
 
-
+let frontCards = ["tarotFront1", "tarotFront2", "tarotFront3"];
 
 // DIXPER SDK INJECTED CLASS
 
@@ -61,29 +71,39 @@ const init = () => {
     createCard();
 }
 
-const createCard = () => {
+const createRandomPosition = () => {
 
-    console.log("HII");
-    let turn = false;
-    const card = new PIXI.Sprite.from(DX_PIXI.resources.tarotFront1.texture);
+}
+
+const createFrontImage = () => {
+
+    let randIdx = Math.floor(Math.rand() * frontCards.length);
+
+    const card = new PIXI.Sprite.from(DX_PIXI.resources.frontCards[randIdx].texture);
     card.x = DX_WIDTH / 2;
     card.y = DX_HEIGHT / 2;
     card.anchor.set(0.5);
     card.zIndex = 99;
+    card.scale.x = 0;
 
-    // card.interactive = true;
-    // card.buttonMode = true;
-    // card.on('pointerdown', onClick);
-
-    // function onClick() {
-    //     console.log("TAP");
-    // }
     DX_LAYERS.ui.addChild(card);
 
+    frontCards.splice(randIdx, 1);
+    return currentValue;
+}
+
+const createCard = () => {
+    let turn = false;
+
+    //CREATE FRONT
+    createFrontImage();
+
+
+    //CREATE BACK
     const button = new dxButton(
-        dixperPluginSample.pixi,
-        "invisibleButton",
-        dixperPluginSample.uiLayer,
+        DX_PIXI,
+        "tarotBack",
+        DX_LAYERS.ui,
         "",
         {
             position: {
@@ -94,29 +114,36 @@ const createCard = () => {
                 x: 1,
                 y: 1,
             },
-            animationSpeed: 0.5,
             hitbox: [-190, -350, 190, -350, 190, 350, -190, 350],
         }
     );
 
+    console.log(button);
+
     button.onClick = (event) => {
-        button.instance.interactive = false;
         turn = true;
-        button.remove();
     };
 
+    let appear = false;
 
     dixperPluginSample.pixi.ticker.add(() => {
-        if (card && turn) {
-            if (card.scale.x < 0) {
-
-                dixperPluginSample.pixi.ticker.stop();
+        if (button && turn) {
+            if (button.instance.scale.x < 0) {
                 turn = false;
+                appear = true;
+
             } else {
-                card.scale.x -= 0.01;
+                button.instance.scale.x -= 0.01;
+            }
+        }
+        else if (card && appear) {
+            if (card.scale.x > 1) {
+                turn = false;
+                appear = false;
+
+            } else {
+                card.scale.x += 0.01;
             }
         }
     });
-
-
 }
