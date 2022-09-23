a720c83ee7ac94f4887840c06a98e01237648d36
adriagonzalez <adria.gonzalez@dixper.gg>
2022-09-23 14:52:38 +0200

    Tarot cards art implemented and memory art implemented
    Note: Back image in memory needs to be changed

--- src/halloween/src/triggered-tarot-card.js
+++ src/halloween/src/triggered-tarot-card.js
@@ -1,23 +1,55 @@
 const images = [
     {
         name: "tarotFront1",
-        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/spritesheets/tarot-front-1.png"
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/images/tarot-card-0001.png"
     },
     {
         name: "tarotFront2",
-        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-front-2.png"
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0002.png"
     },
     {
         name: "tarotFront3",
-        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-front-3.png"
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0003.png"
+    },
+    {
+        name: "tarotFront4",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/images/tarot-card-0004.png"
+    },
+    {
+        name: "tarotFront5",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0006.png"
+    },
+    {
+        name: "tarotFront6",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/images/tarot-card-0007.png"
+    },
+    {
+        name: "tarotFront7",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0008.png"
+    },
+    {
+        name: "tarotFront8",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0009.png"
+    },
+    {
+        name: "tarotFront9",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/images/tarot-card-0010.png"
+    },
+    {
+        name: "tarotFront10",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0011.png"
+    },
+    {
+        name: "tarotFront11",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0012.png"
+    },
+    {
+        name: "tarotFront12",
+        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0013.png"
     },
 ];
 
 const sprites = [
-    {
-        name: "tarotBack",
-        url: "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-rii/src/halloween/assets/spritesheets/tarot-back.json"
-    },
 
     {
         name: "invisibleButton",
@@ -30,8 +62,8 @@ const sounds = [];
 
 // INPUTS PARAMS
 
-let frontCards = ["tarotFront1", "tarotFront2", "tarotFront3"];
-
+let frontCards = ["tarotFront1", "tarotFront2", "tarotFront3", "tarotFront4", "tarotFront5", "tarotFront6", "tarotFront7", "tarotFront8", "tarotFront9", "tarotFront10", "tarotFront11", "tarotFront12"];
+let cardWidth, cardHeigth;
 // DIXPER SDK INJECTED CLASS
 
 const dixperPluginSample = new DixperSDKLib({
@@ -40,6 +72,17 @@ const dixperPluginSample = new DixperSDKLib({
         files: [...images, ...sprites, ...sounds],
     },
 });
+
+const gamePadButtons = [
+    "FACE_1",
+    "FACE_2",
+    "FACE_3",
+    "FACE_4",
+    "RIGHT_SHOULDER",
+    "RIGHT_SHOULDER_BOTTOM",
+    "LEFT_SHOULDER",
+    "LEFT_SHOULDER_BOTTOM",
+];
 
 // INPUTS
 
@@ -51,25 +94,22 @@ const { numCards } =
 dixperPluginSample.onPixiLoad = () => {
     console.clear();
     init();
-    //dixperPluginSample.initChallenge(challengeTitle, challengeTime);
-};
-
-// INIT CHALLENGE
-
-dixperPluginSample.onChallengeAccepted = () => {
-    init();
-};
-
-dixperPluginSample.onChallengeRejected = () => {
-    dixperPluginSample.stopSkill();
-};
-
-dixperPluginSample.onChallengeFinish = () => {
 };
 
 const init = () => {
+    //CARD WIDTH = 275 || SPACE BETWEEN CARDS = 100
+    cardWidth = 275;
+    cardHeigth = 487;
+    let distanceBetweenCards = 25;
+    let totalWidth = cardWidth * numCards + distanceBetweenCards * (numCards - 1);
+    let randLucky = Math.floor(Math.random() * numCards);
     for (var i = 0; i < numCards; i++) {
-        createCard(DX_WIDTH / 2 - 400 + i * 400);
+        if (i === randLucky) {
+            createCard(DX_WIDTH / 2 - totalWidth / 2 + i * (distanceBetweenCards + cardWidth) + cardWidth / 2, i, true);
+        }
+        else {
+            createCard(DX_WIDTH / 2 - totalWidth / 2 + i * (distanceBetweenCards + cardWidth) + cardWidth / 2, i, false);
+        }
     }
 }
 
@@ -91,19 +131,29 @@ const createFrontImage = (posX) => {
     return card
 }
 
-const createCard = (posX) => {
+const createCard = (posX, counter, lucky) => {
     let turn = false;
 
     //CREATE FRONT
     const card = createFrontImage(posX);
 
-    //CREATE BACK
-    const button = new dxButton(
-        DX_PIXI,
-        "tarotBack",
-        DX_LAYERS.ui,
-        "",
+    const button = new DxButton(
+        "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/images/tarot-card-0005.png",
+        ``,
         {
+            isClickable: true,
+            controller: {
+                isPressable: true,
+                button: `${gamePadButtons[counter]}`,
+                x: 0,
+                y: 40,
+            },
+            keyboard: {
+                isPressable: true,
+                button: `${counter + 1}`,
+                x: 0,
+                y: 40,
+            },
             position: {
                 x: posX,
                 y: DX_HEIGHT / 2,
@@ -112,9 +162,12 @@ const createCard = (posX) => {
                 x: 1,
                 y: 1,
             },
-            hitbox: [-190, -350, 190, -350, 190, 350, -190, 350],
+            hitbox: [-cardWidth / 2, -cardHeigth / 2, cardWidth / 2, -cardHeigth / 2, cardWidth / 2, cardHeigth / 2, -cardWidth / 2, cardHeigth / 2],
+            luckyCard: lucky,
         }
     );
+
+    button.start();
 
     button.onClick = (event) => {
         turn = true;
@@ -136,7 +189,7 @@ const createCard = (posX) => {
             if (card.scale.x > 1) {
                 turn = false;
                 appear = false;
-                cardAction(card.name);
+                cardAction(card);
 
             } else {
                 card.scale.x += 0.01;
@@ -145,12 +198,13 @@ const createCard = (posX) => {
     });
 }
 
-const cardAction = (cardName) => {
-    var actionIdx = Number(cardName.substr(cardName.length - 1));
-    if (actionIdx % 2 != 0) {
+const cardAction = (card) => {
+    if (card._options.luckyCard) {
         console.log("WIIII");
+        alert("WIIII");
     }
     else {
         console.log("BOOOH");
+        alert("BOOOH");
     }
 }
