d8c8e7ce925fe5b15e8c173003fd58d86f8cc7bd
adriagonzalez <adria.gonzalez@dixper.gg>
2022-10-05 13:16:38 +0200

    WIP

--- src/halloween/src/challenge-trivial.js
+++ src/halloween/src/challenge-trivial.js
@@ -305,7 +305,7 @@ const init = async () => {
 const loadQuestions = async () => {
   //READ CSV FROM URL AND SAVE IT IN A STRING
   const temp = await fetch(
-    "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/questions-es.csv"
+    "https://raw.githubusercontent.com/Dixper/dixper-sdk-plugin-samples/origin/halloween-skills-adri/src/halloween/assets/questions-en.csv"
   )
     .then((response) => response.text())
     .then((csv) => (readCSV = csv));
@@ -388,6 +388,12 @@ const createQuestionPanel = (questionName) => {
       x: 0.75,
       y: 0.75,
     },
+    text: {
+      fontSize: 20,
+      lineHeight: 20,
+      strokeThickness: 0,
+      dropShadowDistance: 0
+    },
   });
   question.start();
 };
@@ -416,6 +422,12 @@ const createButtonAnswer = () => {
       scale: {
         x: 0.5,
         y: 0.5,
+      },
+      text: {
+        fontSize: 20,
+        lineHeight: 20,
+        strokeThickness: 0,
+        dropShadowDistance: 0
       },
     });
     button.start();
